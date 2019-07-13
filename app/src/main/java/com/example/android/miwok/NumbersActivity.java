f528a0bdc7c8b9efb2569415b18c2e942523d4b9
Ivan <rakonjacivan98@gmail.com>
2019-07-14 01:36:21 +0200

    Dodat Layout za ispisivanje reci u NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -14,5 +17,25 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> listaReci = new ArrayList<String>();
+        listaReci.add("one");
+        listaReci.add("two");
+        listaReci.add("three");
+        listaReci.add("four");
+        listaReci.add("five");
+        listaReci.add("six");
+        listaReci.add("seven");
+        listaReci.add("eight");
+        listaReci.add("nine");
+        listaReci.add("ten");
+
+        LinearLayout rootView=(LinearLayout)findViewById(R.id.rootView);
+
+        for(int i=0;i<10;i++) {
+            TextView txt = new TextView(this);
+            txt.setText(listaReci.get(i));
+            rootView.addView(txt);
+        }
     }
 }
