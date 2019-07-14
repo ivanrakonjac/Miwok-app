a7edcc8fe982d7aae1a8a26a1c007f72db3910ab
Ivan <rakonjacivan98@gmail.com>
2019-07-14 12:23:16 +0200

    ArrayList umesto TextBoxova za brojeve

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,13 +31,22 @@ public class NumbersActivity extends AppCompatActivity {
         listaReci.add("eight");
         listaReci.add("nine");
         listaReci.add("ten");
+        listaReci.add("eleven");
+        listaReci.add("twelve");
+        listaReci.add("threeteen");
+        listaReci.add("fourteen");
+        listaReci.add("fiveteen");
+        listaReci.add("sixteen");
+        listaReci.add("seventeen");
+        listaReci.add("eighteen");
+        listaReci.add("nineteen");
+        listaReci.add("twenty");
 
-        LinearLayout rootView=(LinearLayout)findViewById(R.id.rootView);
 
-        for(int i=0;i<10;i++) {
-            TextView txt = new TextView(this);
-            txt.setText(listaReci.get(i));
-            rootView.addView(txt);
-        }
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listaReci);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
     }
 }
