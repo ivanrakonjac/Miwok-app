e84ef3f5c0e7ed194c7fad79e1db6c3e2fd67497
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2020-10-15 17:38:31 +0200

    Real initial commit

---
+++ app/src/main/java/com/example/miwokapp/MainActivity.java
@@ -0,0 +1,22 @@
+
+package com.example.miwokapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+    }
+}
